fad7e143048953e3972697b0aa0d865fee24f580
Marcos Simoneli <marcossimoneli@hotmail.com>
2025-01-14 20:42:47 -0300

    Add importacao route

--- fastapi-app/src/main.py
+++ fastapi-app/src/main.py
@@ -3,6 +3,7 @@ from routes.hello import router as hello_router
 from routes.processamento import router as processamento_router
 from routes.producao import router as producao_router
 from routes.comercio import router as comercio_router
+from routes.importacao import router as importacao_router
 
 def create_app():
     app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
@@ -10,6 +11,7 @@ def create_app():
     app.include_router(processamento_router, prefix="/embrapa/processamento")
     app.include_router(producao_router, prefix="/embrapa/producao")
     app.include_router(comercio_router, prefix="/embrapa/comercio")
+    app.include_router(importacao_router, prefix="/embrapa/importacao")
     return app
 
 app = create_app()
