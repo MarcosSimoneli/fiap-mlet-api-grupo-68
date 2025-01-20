81c88fdaca776fce3942f15dde92d66a60eb47e4
Matheo Bardaquim <matheo.bardaquim@gmail.com>
2025-01-20 19:42:13 -0300

    hospendando no vercel

--- fastapi-app/src/main.py
+++ fastapi-app/src/main.py
@@ -7,6 +7,8 @@ from routes.producao import router as producao_router
 from routes.comercio import router as comercio_router
 from routes.importacao import router as importacao_router
 from routes.exportacao import router as exportacao_router
+from fastapi.middleware.cors import CORSMiddleware
+
 class TokenData(BaseModel):
     username: str
     password: str
@@ -14,12 +16,20 @@ class TokenData(BaseModel):
 def create_app():
     app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Permite acesso de qualquer origem
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     @app.post("/token")
     def generate_token(data: TokenData):
         token = create_access_token(data.dict())
         return {"access_token": token, "token_type": "bearer"}
 
-    app.include_router(hello_router, prefix="/hello")
+    app.include_router(hello_router, prefix="")
     app.include_router(processamento_router, prefix="/embrapa/processamento", dependencies=[Depends(validate_token)])
     app.include_router(producao_router, prefix="/embrapa/producao", dependencies=[Depends(validate_token)])
     app.include_router(comercio_router, prefix="/embrapa/comercio", dependencies=[Depends(validate_token)])
